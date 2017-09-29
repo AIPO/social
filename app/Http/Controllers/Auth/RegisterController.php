934d57c09d069c6fc78a1cda75088325200cdf17
povilas <p.vaitonis@gmail.com>
2017-09-29 15:59:56 +0300

    controler typo fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        if(data['gender']){
+        if($data['gender']){
             $avatar='public/defaults/avatars/male.png';
         }
         else{
